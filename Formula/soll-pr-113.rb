3bd64f98ab06280d5593ddeb37f1a2c0a247ef72
Soll CI <noreply@github.com>
2026-04-30 23:11:38 +0000

    soll-pr-113 @ 77ebc9bc

--- Formula/soll-pr-113.rb
+++ Formula/soll-pr-113.rb
@@ -1,21 +1,21 @@
 class SollPr113 < Formula
   desc "Soll preview build (PR #113)"
   homepage "https://github.com/marginallyuseful/soll"
-  version "0.0.0-pr.113.77fafa53"
+  version "0.0.0-pr.113.77ebc9bc"
   license :cannot_represent
 
   on_macos do
-    url "https://github.com/marginallyuseful/homebrew-soll-preview/releases/download/pr-113-77fafa53/soll-pr-113-macos-arm64"
+    url "https://github.com/marginallyuseful/homebrew-soll-preview/releases/download/pr-113-77ebc9bc/soll-pr-113-macos-arm64"
     sha256 "4a9a129a8faf65544ae4af103950fbea71f2162546b6e939568f3d8052af3c02"
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/marginallyuseful/homebrew-soll-preview/releases/download/pr-113-77fafa53/soll-pr-113-linux-arm64"
+      url "https://github.com/marginallyuseful/homebrew-soll-preview/releases/download/pr-113-77ebc9bc/soll-pr-113-linux-arm64"
       sha256 "de7572849640487a378992249da37e5b49574ed6d29222241165ef89900630c8"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/marginallyuseful/homebrew-soll-preview/releases/download/pr-113-77fafa53/soll-pr-113-linux-x64"
+      url "https://github.com/marginallyuseful/homebrew-soll-preview/releases/download/pr-113-77ebc9bc/soll-pr-113-linux-x64"
       sha256 "3963eccd1a8b8426364213749d3ecaffafeda2391bcfd229b45efe5d83fb896c"
     end
   end
